a580e6f7394c15f3615e809fbe6b4719cf33f596
Ghaiath <gm.trabulsi@gmail.com>
2018-10-28 13:56:00 +0100

    Bring xml file and save it in a document variable.

--- Lernia_Datorkommunikation_Project/src/main/java/main/BringRates.java
+++ Lernia_Datorkommunikation_Project/src/main/java/main/BringRates.java
@@ -1,15 +1,20 @@
 package main;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactoryConfigurationError;
 import javax.xml.xpath.XPathExpressionException;
 
+import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
 public class BringRates {
@@ -23,6 +28,15 @@ public class BringRates {
 
 	Connection conn = DriverManager.getConnection(dbUrl, uname, pwd);
 	conn.createStatement().executeUpdate(query);
+	
+	String XMLpath = "E:\\System Integtator\\Data communication project\\2.xml";
+	File file = new File(XMLpath);
+	System.out.println(file.getAbsoluteFile());
+	DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+	factory.setNamespaceAware(true);
+	DocumentBuilder builder = factory.newDocumentBuilder();
+	Document xmlDoc = builder.parse(new URL("https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml").openStream());
+	//clean(xmlDoc);
 	}
 
 }
